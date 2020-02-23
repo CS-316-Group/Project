08c93ee1b11b9af230038c8268c088d378dc3e4d
Caroline <carolinewang01@gmail.com>
2020-02-23 13:20:12 -0500

    uploaded automatic credential loading

---
+++ src/utils/get_project_directory.py
@@ -0,0 +1,13 @@
+import os
+import re
+
+
+def get_project_directory() -> list:
+    """ Returns project directory
+        Returns:
+            project_dir (list): path back to top level directory
+    """
+    project_dir = re.split(r"\\|/",
+                           os.path.dirname(os.path.realpath(__file__))) + ['..', '..']
+
+    return project_dir
---
+++ src/utils/load_confs.py
@@ -0,0 +1,56 @@
+import yaml
+import os
+import re
+from utils.get_project_directory import get_project_directory
+
+
+project_dir = get_project_directory()
+
+
+def collapse_dict_hierarchy(nested_dict: dict):
+    collapsed_dict = {}
+    for name, subdict in nested_dict.items():
+        collapsed_dict = {**collapsed_dict, **subdict}
+    return collapsed_dict
+
+
+def load_parameters():
+    filepath = os.sep.join(project_dir + 'conf/base/parameters.yml'.split('/'))
+    with open(filepath) as f:
+        params = yaml.safe_load(f)
+    return collapse_dict_hierarchy(params)
+
+
+def load_app_parameters():
+    filepath = os.sep.join(
+        project_dir + 'conf/base/app_parameters.yml'.split('/'))
+
+    with open(filepath) as f:
+        params = yaml.safe_load(f)
+    return collapse_dict_hierarchy(params)
+
+
+def load_credentials():
+
+    filepath = os.sep.join(
+        project_dir + 'conf/local/credentials.yml'.split('/'))
+
+    with open(filepath) as f:
+        creds = yaml.safe_load(f)
+
+    return creds
+
+
+def load_paths():
+    filepath = os.sep.join(project_dir + 'conf/base/paths.yml'.split('/'))
+
+    with open(filepath) as f:
+        paths = yaml.safe_load(f)
+        # example of how to deal with mac vs windows folder structure 
+        # s3_paths = paths['s3_paths']
+        # local_paths = paths['local_paths']
+        # db_paths = paths['db_paths']
+
+    # for key, val in local_paths.items():
+        # local_paths[key] = os.sep.join(project_dir + re.split(r"\\|/", val))
+    return paths
