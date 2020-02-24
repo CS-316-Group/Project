4bebc1ab9af4d7b89a49ce40aa9c1179734bc038
Martha Aboagye <46540925+AbMartha@users.noreply.github.com>
2020-02-23 23:16:34 -0500

    Create load.sql
    load.sql at milestone1

---
+++ Sample SQL Files/load.sql
@@ -0,0 +1,32 @@
+INSERT INTO artists VALUES('17hR0sYHpx7VYTMRfFUOmY', 'Maurice Ravel', 'classical', 99, 19220102, 'https://www.google.com/search?q=justin+bieber&hl=en&sxsrf=ALeKk03RP_Rg2nypDajLSHhI9beAernYMg:1582496684412&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi46-3F2-jnAhUOVt8KHUI_B-0Q_AUoAnoECBwQBA&biw=959&bih=735#imgrc=HsQOm9N2LV3vdM');
+INSERT INTO artists VALUES('6Sqhwn4JweupJ3sn9zHNsq', 'Amandine Savary', 'classical', 89, 1920102, 'https://www.google.com/search?q=justin+bieber&hl=en&sxsrf=ALeKk03RP_Rg2nypDajLSHhI9beAernYMg:1582496684412&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi46-3F2-jnAhUOVt8KHUI_B-0Q_AUoAnoECBwQBA&biw=959&bih=735#imgrc=HsQOm9N2LV3vdM');
+INSERT INTO artists VALUES('3qprTw1SHzbubNcJPqFaFS', 'Vineta Sareika', 'pop, folk, hiphop', 60, 420102, 'https://www.google.com/search?q=justin+bieber&hl=en&sxsrf=ALeKk03RP_Rg2nypDajLSHhI9beAernYMg:1582496684412&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi46-3F2-jnAhUOVt8KHUI_B-0Q_AUoAnoECBwQBA&biw=959&bih=735#imgrc=HsQOm9N2LV3vdM');
+INSERT INTO artists VALUES('5nRDtK82lHmYRXnKjAzI9j', 'Christian-Pierre La Marca', 'metal, hiphop', 40, 890102, 'https://www.google.com/search?q=justin+bieber&hl=en&sxsrf=ALeKk03RP_Rg2nypDajLSHhI9beAernYMg:1582496684412&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi46-3F2-jnAhUOVt8KHUI_B-0Q_AUoAnoECBwQBA&biw=959&bih=735#imgrc=HsQOm9N2LV3vdM');
+INSERT INTO artists VALUES('6UE7nl9mha6s8z0wFQFIZ2', 'Robyn', 'pop, dance', 95, 12890102, 'https://www.google.com/search?q=justin+bieber&hl=en&sxsrf=ALeKk03RP_Rg2nypDajLSHhI9beAernYMg:1582496684412&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi46-3F2-jnAhUOVt8KHUI_B-0Q_AUoAnoECBwQBA&biw=959&bih=735#imgrc=HsQOm9N2LV3vdM');
+INSERT INTO listeners VALUES('314xcqarki42gnkosjtfplluumya', 'Ilovemusic', 2, 'https://platform-lookaside.fbsbx.com/platform/...');
+INSERT INTO listeners VALUES('314xcqarki42gnkosjtfplluumyb', 'what a wonderful word', 10, 'https://platform-lookaside.fbsbx.com/platform/...');
+INSERT INTO listeners VALUES ('314xcqarki42gnkosjtfplluumyc', 'Popmusicforever', 20, 'hhjirhuwhjkhuywr');
+INSERT INTO tracks VALUES('0xH4hjpQfNInY3lqC3Osc1', 'Trio pour piano violon & violoncelle: II. Pant...', 10, 'https://p.scdn.co/mp3-preview/c822c5e60fdc9cf4...');
+INSERT INTO tracks VALUES('1RI3rY6Y5Shrr2pFCO1xmu', 'Beach2k20 - Yaeji Remix', 36, 'https://p.scdn.co/mp3-preview/08be7225f817da6f...');
+INSERT INTO tracks VALUES('2Fxmhks0bxGSBdJ92vM42m', 'bad guy', 94, 'https://p.scdn.co/mp3-preview/11fadb5f777b56f8...');
+INSERT INTO tracks VALUES('39nTNk5MfyiiRupTwn1iyC', 'Take It Out On Me', 77, 'https://p.scdn.co/mp3-preview/3acbfa9184485bd6...	');
+INSERT INTO tracks VALUES('7BlBVFwvbWvcwNcUarQmjk', 'Yo x Ti Tu x Mi', 85, 'https://p.scdn.co/mp3-preview/3acbfa9184485bd6...	');
+INSERT INTO albums VALUES('3FhRqpxS0Rlx0f4RtZMaBh', 'Ravel: Trio Sonate pour violon et violoncelle ...','ALBUM','https://i.scdn.co/image/ab67616d0000b27309302d...');
+INSERT INTO albums VALUES('5mHUgXZKe4szVeCMBz9MNu', 'Beach2k20 (Yaeji Remix)','SINGLE','https://i.scdn.co/image/ab67616d0000b27309302d...');
+INSERT INTO albums VALUES('0S0KGZnfBGSIssfF54WSJh', 'WHEN WE ALL FALL ASLEEP WHERE DO WE GO?','ALBUM','https://i.scdn.co/image/ab67616d0000b27309302d...');
+INSERT INTO albums VALUES('63iWSELt9V1kV6RSMxN7Ii', 'Changes','ALBUM','https://i.scdn.co/image/ab67616d0000b27309302d...');
+INSERT INTO albums VALUES('3844bY26oeSkqd06th4EYp', 'Yo x Ti Tu x Mi','SINGLE','https://i.scdn.co/image/ab67616d0000b27309302d...');
+INSERT INTO topartists VALUES ('314xcqarki42gnkosjtfplluumya','5nRDtK82lHmYRXnKjAzI9j' , 'short_term');
+INSERT INTO topartists VALUES ('314xcqarki42gnkosjtfplluumyb','5nRDtK82lHmYRXnKjAzI9j' , 'short_term');
+INSERT INTO topartists VALUES ('314xcqarki42gnkosjtfplluumyb','6Sqhwn4JweupJ3sn9zHNsq' , 'long_term');
+INSERT INTO toptracks VALUES ('314xcqarki42gnkosjtfplluumyb','0xH4hjpQfNInY3lqC3Osc1','short_term');
+INSERT INTO toptracks VALUES ('314xcqarki42gnkosjtfplluumyb', '1RI3rY6Y5Shrr2pFCO1xmu', 'long_term');
+INSERT INTO toptracks VALUES ('314xcqarki42gnkosjtfplluumyc','7BlBVFwvbWvcwNcUarQmjk', 'long_term');
+INSERT INTO createdby VALUES('1RI3rY6Y5Shrr2pFCO1xmu', '3qprTw1SHzbubNcJPqFaFS');
+INSERT INTO createdby VALUES('2Fxmhks0bxGSBdJ92vM42m', '6UE7nl9mha6s8z0wFQFIZ2');
+INSERT INTO createdby VALUES('1RI3rY6Y5Shrr2pFCO1xmu', '6UE7nl9mha6s8z0wFQFIZ2');
+INSERT INTO albumcontainstrack VALUES('5mHUgXZKe4szVeCMBz9MNu', '3qprTw1SHzbubNcJPqFaFS');
+INSERT INTO albumcontainstrack VALUES('3FhRqpxS0Rlx0f4RtZMaBh', '6UE7nl9mha6s8z0wFQFIZ2');
+
+
+
