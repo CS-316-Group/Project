a42b9e2f1527e5ccfd3db61ec220b88d4fb581d9
AbMartha <martha.aboagye@duke.edu>
2020-03-28 20:19:20 -0400

    deleted local src folder from our app not needed

--- src/d04_app/src/config.py
+++
@@ -1,4 +0,0 @@
-#SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://user:password@url/database'
-SQLALCHEMY_DATABASE_URI = 'postgres://dbuser:example@vcm-13360.vm.duke.edu/production'
-SQLALCHEMY_ECHO = True
-DEBUG = True
