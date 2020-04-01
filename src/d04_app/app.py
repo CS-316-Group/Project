47a246f357c0fd6f010854eaa19563e5ae83376f
AbMartha <martha.aboagye@duke.edu>
2020-04-01 09:43:11 -0400

    fixed redirection for artists form

--- src/d04_app/app.py
+++ src/d04_app/app.py
@@ -11,14 +11,13 @@ import d04_app.authentication as authentication
 # from d01_data_processing.data_cleaning import *	
 # from d01_data_processing.spotify_user import SpotifyUser	
 # from d00_utils.load_confs import load_credentials	
-
 	
 app = Flask(__name__)
 app.secret_key = 'cs316'
 app.config.from_object('d04_app.config')
 db = SQLAlchemy(app, session_options={'autocommit': False})
 
-import d04_app.models as models
+import d04_app.models
 
 @app.route('/')
 def home():
@@ -31,13 +30,16 @@ def welcome():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    
-
     form = forms.LoginForm()
-
     if form.validate_on_submit():
         response = startup.getUser()
         return redirect(response)
+
+    return render_template('login.html', form=form)
+
+    #if form.validate_on_submit():
+    #response = startup.getUser()
+    #return redirect(response)
     
     #when we change how spotifyUser is initialized. 
     #new_user = SpotifyUser(token,	new_username)
@@ -63,13 +65,13 @@ def callback():
 
 @app.route('/database', methods=['GET', 'POST'])
 def database():
-    artist_names = db.session.query(models.Artists.artist_name) 
+    artist_names = db.session.query(d04_app.models.Artists.artist_name) 
     dropdown_list = []
     for artist in artist_names:
         dropdown_list.append(artist[0])
     form = forms.artistsform.form(dropdown_list)
     if form.validate_on_submit():
-        return render_template('artistpage.html') # not sure if this is right
+        return redirect('/artistpage') # not sure if this is right
     return render_template('database.html', dropdown_list=dropdown_list, form=form)
 
 
